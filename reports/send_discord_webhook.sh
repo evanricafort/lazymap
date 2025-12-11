3651051d1bf2bcea749132b03421a16cba5d373a
Evan Ricafort <evan.ricafort@gmail.com>
2025-12-11 12:53:41 +0800

    Update send_discord_webhook.sh

--- reports/send_discord_webhook.sh
+++ reports/send_discord_webhook.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
-# reports/send_discord_webhook.sh
-# Sends the HTML report to a specified Discord webhook.
-
 source "lib/colors.sh"
 
 send_discord_webhook() {
-    local report_file=$1
+    local scan_dir=$1
     local webhook_url=$2
     local start_date=$3
 
-    if [[ -n "$webhook_url" && -f "$report_file" ]]; then
+    if [[ -n "$webhook_url" && -d "$scan_dir" ]]; then
         echo -e "${YELLOW}Sending report to discord...${NC}\n"
 
-        # Check if curl is installed
         if ! command -v curl &> /dev/null; then
             echo -e "${RED}Error: 'curl' command not found. Please install it to use the Discord webhook feature.${NC}\n"
             return 1
         fi
 
-        # Send the file to the Discord webhook
+        if ! command -v zip &> /dev/null; then
+            echo -e "${RED}Error: 'zip' command not found. Please install it to use the Discord webhook feature.${NC}\n"
+            return 1
+        fi
+
+        local zip_file="${scan_dir%/}.zip"
+        zip -r -q "$zip_file" "$scan_dir"
+
         curl -X POST -H "Content-Type: multipart/form-data" \
-             -F "file=@$report_file" \
+             -F "file=@$zip_file" \
              -F "payload_json={\"content\":\"**Lazymap Scan Report**\\nScan started on: \`$start_date\`\"}" \
              "$webhook_url" > /dev/null 2>&1
 
